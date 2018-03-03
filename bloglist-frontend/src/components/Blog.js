c5092be3bb95c5afab6a6ff80ece81b15b3851d3
Sari R <sariraut@dhcp-asv-101.helsinki.fi>
2018-03-03 18:00:24 +0200

    teht5_6

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import Togglable from '../components/Togglable'
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5
+}
+
 const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+
+  <div style={blogStyle}>
+    <Togglable buttonLabel={blog.title}>
+     <div>{blog.author}</div>
+     <div>{blog.url}</div>
+     <div>{blog.likes} likes</div>
+     <button> like </button>
+     <div>Added by {blog.user.name}</div>
+     </Togglable>
   </div>  
 )
 
